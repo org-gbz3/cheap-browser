cc8f1bb834250a425281652c40a61feda95f6b52
gbz3 <1808483+gbz3@users.noreply.github.com>
2026-05-28 11:06:45 +0000

    1.6 HTMLの表示

--- browser/main.py
+++ browser/main.py
@@ -1,2 +1,68 @@
+import socket
+
+
+class URL:
+    def __init__(self, url: str) -> None:
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http"
+
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+    def request(self) -> str:
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        s.connect((self.host, 80))
+
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+        print(f"ver={version} stat={status} exp={explanation}")
+
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        content = response.read()
+        s.close()
+
+        return content
+
+
+def show(body: str):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+def load(url: URL):
+    body = url.request()
+    show(body)
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    import sys
+
+    load(URL(sys.argv[1]))
