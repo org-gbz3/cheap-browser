c08b5cd97fbe5899352bd04166932b4d81acf13c
gbz3 <1808483+gbz3@users.noreply.github.com>
2026-05-29 23:10:13 +0000

    2.4 テキストのスクロール

--- browser/main.py
+++ browser/main.py
@@ -113,6 +113,21 @@ WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 
 
+def layout(text: str) -> list[tuple[int, int, str]]:
+    display_list: list[tuple[int, int, str]] = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
+
+
+SCROLL_STEP = 100
+
+
 class Browser:
     def __init__(self) -> None:
         self.window = tkinter.Tk()
@@ -122,17 +137,23 @@ class Browser:
             height=HEIGHT,
         )
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def load(self, url: URL):
         body = url.request()
         text = lex(body)
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+        self.display_list = layout(text)
+        self.draw()
+
+    def scrolldown(self, e: tkinter.Event):
+        self.scroll += SCROLL_STEP
+        self.draw()
 
 
 if __name__ == "__main__":
