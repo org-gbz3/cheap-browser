dadf6d9a916a02645c21eee8f051bb392e7e1521
gbz3 <1808483+gbz3@users.noreply.github.com>
2026-06-07 01:49:26 +0000

    6.4 スタイルシートの適用

--- browser/main.py
+++ browser/main.py
@@ -13,6 +13,7 @@ from zoneinfo import ZoneInfo
 
 type DisplayItem = tuple[int, int, str, tkinter.font.Font]
 type DrawItem = DrawText | DrawRect
+type CssRules = list[tuple[TagSelector | DescendantSelector, dict[str, str]]]
 
 
 # JST タイムゾーンの設定
@@ -85,6 +86,24 @@ class URL:
 
         return content
 
+    def resolve(self, url: str):
+        if "://" in url:
+            return URL(url)
+        if not url.startswith("/"):
+            dir, _ = self.path.rsplit("/", 1)
+            while url.startswith("../"):
+                _, url = url.split("/", 1)
+                if "/" in dir:
+                    dir, _ = dir.rsplit("/", 1)
+            url = dir + "/" + url
+        if url.startswith("//"):
+            return URL(self.scheme + ":" + url)
+        else:
+            return URL(self.scheme + "://" + self.host + ":" + str(self.port) + url)
+
+    def __repr__(self) -> str:
+        return f"{self.scheme}://{self.host}:{self.port}{self.path}"
+
 
 class Text:
     def __init__(self, text: str, parent: Element) -> None:
@@ -119,6 +138,13 @@ def print_layout_tree(node: DocumentLayout | BlockLayout, indent: int = 0):
     print(" " * indent, node)
     for child in node.children:
         print_layout_tree(child, indent + 2)
+
+
+def tree_to_list(tree: Element | Text, list: list[Element | Text]):
+    list.append(tree)
+    for child in tree.children:
+        tree_to_list(child, list)
+    return list
 
 
 class HTMLParser:
@@ -300,7 +326,7 @@ class CSSParser:
 
     def body(self) -> dict[str, str]:
         pairs: dict[str, str] = {}
-        while self.i < len(self.s):
+        while self.i < len(self.s) and self.s[self.i] != "}":
             try:
                 prop, val = self.pair()
                 pairs[prop.casefold()] = val
@@ -308,7 +334,7 @@ class CSSParser:
                 self.literal(";")
                 self.whitespace()
             except Exception:
-                why = self.ignore_until([";"])
+                why = self.ignore_until([";", "}"])
                 if why == ";":
                     self.literal(why)
                     self.whitespace()
@@ -323,6 +349,69 @@ class CSSParser:
             else:
                 self.i += 1
         return None
+
+    def selector(self):
+        out = TagSelector(self.word().casefold())
+        self.whitespace()
+        while self.i < len(self.s) and self.s[self.i] != "{":
+            tag = self.word()
+            descendant = TagSelector(tag.casefold())
+            out = DescendantSelector(out, descendant)
+            self.whitespace()
+        return out
+
+    def parse(self):
+        rules: CssRules = []
+        while self.i < len(self.s):
+            try:
+                self.whitespace()
+                selector = self.selector()
+                self.literal("{")
+                self.whitespace()
+                body = self.body()
+                self.literal("}")
+                rules.append((selector, body))
+            except Exception:
+                why = self.ignore_until(["}"])
+                if why == "}":
+                    self.literal("}")
+                    self.whitespace()
+                else:
+                    break
+        return rules
+
+
+class TagSelector:
+    def __init__(self, tag: str):
+        self.tag = tag
+
+    def __repr__(self) -> str:
+        return self.tag
+
+    def matches(self, node: Element | Text):
+        return isinstance(node, Element) and self.tag == node.tag
+
+
+class DescendantSelector:
+    def __init__(self, ancestor: TagSelector | DescendantSelector, descendant: TagSelector):
+        self.ancestor = ancestor  # 先祖
+        self.descendant = descendant  # 子孫
+
+    def __repr__(self) -> str:
+        return f"{self.ancestor} {self.descendant}"
+
+    def matches(self, node: Element | Text) -> bool:
+        # `p a` の a か？
+        if not self.descendant.matches(node):
+            return False
+
+        while node.parent:
+            # root まで親を辿り、一致する先祖を探す
+            if self.ancestor.matches(node.parent):
+                return True
+            node = node.parent
+
+        return False
 
 
 FONTS: dict[
@@ -346,15 +435,28 @@ def get_font(
     return FONTS[key][0]
 
 
-def style(node: Element | Text):
+DEFAULT_STYLE_SHEET = CSSParser(open("browser/browser.css").read()).parse()
+
+
+def style(node: Element | Text, rules: CssRules):
     node.style = {}
+
+    # スタイルシートをWebページに適用。
+    for selector, body in rules:
+        if not selector.matches(node):
+            continue
+        for prop, val in body.items():
+            node.style[prop] = val
+
+    # style属性をパースしWebページに適用。
+    # スタイルシートで定義されたスタイルは上書き。
     if isinstance(node, Element) and "style" in node.attributes:
         pairs = CSSParser(node.attributes["style"]).body()
         for property, value in pairs.items():
             node.style[property] = value
 
     for child in node.children:
-        style(child)
+        style(child, rules)
 
 
 WIDTH, HEIGHT = 800, 600
@@ -680,7 +782,25 @@ class Browser:
         if self.html_tree:
             print_html_tree(self.nodes)
 
-        style(self.nodes)
+        css_rules = DEFAULT_STYLE_SHEET.copy()
+        links = [
+            node.attributes["href"]
+            for node in tree_to_list(self.nodes, [])
+            if isinstance(node, Element)
+            and node.tag == "link"
+            and node.attributes.get("rel") == "stylesheet"
+            and "href" in node.attributes
+        ]
+        for link in links:
+            style_url = url.resolve(link)
+            logging.info(f"css found. [{style_url}]")
+            try:
+                body = style_url.request()
+            except:
+                continue
+            css_rules.extend(CSSParser(body).parse())
+        logging.info(f"css = {css_rules}")
+        style(self.nodes, css_rules)
 
         self.document = DocumentLayout(self.nodes)
         self.document.layout()
